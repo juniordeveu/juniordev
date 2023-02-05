a5196d1d1b469730a0840f7457f971ae9d69d2e1
juniordeveu <tomrys@juniordev.eu>
2023-02-05 19:09:04 +0100

    add section and anim

--- animation.js
+++ animation.js
@@ -9,9 +9,19 @@ let show = function(){
     } else{
             getElement[0].classList.remove( 'show' ); 
         };
-    
-
-
 }
 
-window.addEventListener("scroll",show)
+let show2 = function() {
+        let getElementSection = document.querySelectorAll( '.largeTwo' );
+        let wH = window.innerHeight;
+        let getPositionTop = getElementSection[0].getBoundingClientRect().top;
+        let point = 100;
+        if (getPositionTop < wH - point ) {
+            getElementSection[0].classList.add( 'showSection' );
+        } else {
+            getElementSection[0].classList.remove( 'showSection' );
+        }
+    
+    }
+    window.addEventListener( "scroll", show );
+    window.addEventListener( "scroll", show2 );
